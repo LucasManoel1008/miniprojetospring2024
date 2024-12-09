6ea072df5f13032f517cae39ebdccd8873fbc592
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-12-09 08:40:19 -0300

    Update CORS configuration in UsuarioController to restrict origins

--- src/main/java/br/com/itb/miniprojetospring/control/UsuarioController.java
+++ src/main/java/br/com/itb/miniprojetospring/control/UsuarioController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600, allowCredentials = "false")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired

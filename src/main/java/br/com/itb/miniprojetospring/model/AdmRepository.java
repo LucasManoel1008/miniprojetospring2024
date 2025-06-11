467bd4e171391efe9e0f1a7f622b8986db5e13de
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2025-06-11 07:47:35 -0300

    refactor: Remove AdmController, Adm model, AdmRepository, and AdmService as part of code cleanup

--- src/main/java/br/com/itb/miniprojetospring/model/AdmRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.itb.miniprojetospring.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdmRepository extends JpaRepository<Adm, String> {
-    Adm findByNome(String name);
-}
